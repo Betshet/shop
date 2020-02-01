dfa4979c4b5299297fc1fdaf1113f46ca7d30f2a
Betshet <45632828+Betshet@users.noreply.github.com>
2020-02-01 15:23:39 +0100

    fixed negative value check

--- src/main/java/shop/view/DetailWindow.java
+++ src/main/java/shop/view/DetailWindow.java
@@ -63,8 +63,7 @@ public class DetailWindow extends JFrame {
 					 
 						daoArticle.editStock(article, value);
 					} catch (Exception e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
+						ErrorPopup err = new ErrorPopup("Invalid value");
 					}
 					
 			}
